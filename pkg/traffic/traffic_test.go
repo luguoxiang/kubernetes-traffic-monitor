981fb806a31bd5ec4ceb936bd82e8e87770cfd2b
Guoxiang Lu <guoxiang.lu@oracle.com>
2019-07-23 11:29:15 +0800

    add test

--- pkg/traffic/traffic_test.go
+++ pkg/traffic/traffic_test.go
@@ -62,3 +62,55 @@ func TestTrafficManager(t *testing.T) {
 	assert.Nil(t, tm.allPackets[5000].Next)
 	assert.Nil(t, tm.allRequests[123])
 }
+
+func TestTrafficManagerGetRequest(t *testing.T) {
+	tm := TrafficManager{}
+	t1 := TrafficInfo{
+		SrcIP:                "10.1.1.1",
+		DstIP:                "10.1.2.2",
+		SrcPort:              123,
+		DstPort:              456,
+		TcpRequestTimestamp:  []byte{1, 2, 3},
+		requestTimestampNano: 5 * 1e9,
+	}
+	tm.AddRequest(&t1)
+
+	t2 := TrafficInfo{
+		SrcIP:                "10.1.1.1",
+		DstIP:                "10.1.2.2",
+		SrcPort:              123,
+		DstPort:              456,
+		TcpRequestTimestamp:  []byte{1, 2, 4},
+		requestTimestampNano: 5 * 1e9,
+	}
+	tm.AddRequest(&t2)
+	//source is unknown, do not match src ip
+	result, duplicate := tm.GetRequest("", 123, "10.1.2.2", 456, []byte{1, 2, 3})
+	assert.Equal(t, result, &t2)
+	assert.False(t, duplicate)
+
+	t2.Src = "test"
+	result, duplicate = tm.GetRequest("", 123, "10.1.2.2", 456, []byte{1, 2, 3})
+	assert.Equal(t, result, &t1)
+	assert.False(t, duplicate)
+	t1.Src = "test"
+	//source is not unknown, should match src ip
+	result, duplicate = tm.GetRequest("", 123, "10.1.2.2", 456, []byte{1, 2, 3})
+	assert.Nil(t, result)
+
+	result, duplicate = tm.GetRequest("10.1.1.1", 123, "10.1.2.2", 456, []byte{1, 2, 3})
+	assert.Equal(t, result, &t2)
+	assert.False(t, duplicate)
+
+	t2.TcpResponseTimestamp = []byte{1, 2, 3}
+	result, duplicate = tm.GetRequest("10.1.1.1", 123, "10.1.2.2", 456, []byte{1, 2, 3})
+	assert.Nil(t, result)
+	assert.True(t, duplicate)
+
+	t1.TcpResponseTimestamp = []byte{1, 2, 4}
+	t2.TcpResponseTimestamp = []byte{1, 2, 4}
+	//should match oldest response
+	result, duplicate = tm.GetRequest("10.1.1.1", 123, "10.1.2.2", 456, []byte{1, 2, 3})
+	assert.Equal(t, result, &t1)
+	assert.False(t, duplicate)
+}
